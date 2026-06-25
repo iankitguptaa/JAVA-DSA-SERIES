489408429d2aaac77530da5aab00a9a4dae97526
Ankit Gupta <iankitgupta04@gmail.com>
2026-06-25 06:02:01 +0530

    fix

--- ArrayProblem1.java
+++ ArrayProblem1.java
@@ -1,3 +1,4 @@
+
 public class ArrayProblem1 {
     static double getAverage(int[] arr) {
         double sum = 0;
@@ -21,16 +22,32 @@ public class ArrayProblem1 {
         return newArray;
     }
 
+    static boolean LinearSearch(int arr[], int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        // 1. Find the average of array elements
+        // 1. Find the average of array elements.
         // int[] arr = { 2, 4, 1, 3 };
         // System.out.println(getAverage(arr));
 
-        int arr[] = { 1, 2, 3, 4, 5 };
-        int ans[] = multiply(arr);
-        System.out.println("Printing ans array: ");
-        for (int i : ans) {
-            System.out.println(i);
-        }
+        // 2. Multiply each element of array by 10.
+        // int arr[] = { 1, 2, 3, 4, 5 };
+        // int ans[] = multiply(arr);
+        // System.out.println("Printing ans array: ");
+        // for (int i : ans) {
+        // System.out.println(i);
+        // }
+
+        // 3. Linear Search
+        // int arr[] = { 3, 4, 6, 1, 2 };
+        // boolean ans = LinearSearch(arr, 6);
+        // System.out.println(ans);
+
     }
 }
